fad8da63f945da7f2057348baa52e6ac3c8116b7
Monaka <yuekay88@uga.edu>
2021-10-08 23:31:23 -0400

    Data Exploration Commit (07)

--- README.md
+++ README.md
@@ -3,10 +3,19 @@
 This is Monica Chan's class project repository.
 
 __2021.09.16 - Part 1 -__  
+
 * Required assignment outputs in the products folder in Part_1 as "Proposal.Rmd"" -- knitted to both document and [html](/products/Part_1/Proposal.html) outputs.
+
 * Current data has been uploaded in data folder as 2021.08.18_Spike.csv.
 
-2021.10.08 - Part 2 -  
+__2021.10.08 - Part 2 -__  
+
+* Updated data was included in raw data. 
+* Raw data was processed and saved in processed_data folder. Created additional RDS file for Undetected entries.  processing_code file contains processing script
+* analysis_code file contains analysis script
+* Data wrangling can be found in Products folder Part_2 called "DataExploration.Rmd"
+
+
 2021.10.29 - Part 3 -  
 2021.11.26 - Part 4 -  
 2021.12.10 - Part 5 -  
--- code/analysis_code/analysisscript.R
+++ code/analysis_code/analysisscript.R
@@ -12,16 +12,18 @@ library(here) #for data loading/saving
 #path to data
 #note the use of the here() package and not absolute paths
 data_location <- here::here("data","processed_data","processeddata.rds")
+data_location2 <- here::here("data","processed_data","UndeterminedData.rds")
+
+
 
 #load data. 
 mydata <- readRDS(data_location)
-
+Un_mydata <- readRDS(data_location2)
 ######################################
 #Data exploration/description
 ######################################
-#I'm using basic R commands here.
-#Lots of good packages exist to do more.
-#For instance check out the tableone or skimr packages
+
+
 
 #summarize data 
 mysummary = summary(mydata)
@@ -40,7 +42,12 @@ saveRDS(summary_df, file = summarytable_file)
 
 #make a scatterplot of data
 #we also add a linear regression line to it
-p1 <- mydata %>% ggplot(aes(x=Height, y=Weight)) + geom_point() + geom_smooth(method='lm')
+p1 <- mydata %>% 
+  filter(Method!="PCR")%>%
+  ggplot(aes(x=Dilution.Factor, y=Ct)) + 
+  geom_jitter(aes(color=Method)) + 
+  geom_smooth(method='lm')+
+  facet_grid(Method~Type)
 
 #look at figure
 plot(p1)
@@ -54,7 +61,7 @@ ggsave(filename = figure_file, plot=p1)
 ######################################
 
 # fit linear model
-lmfit <- lm(Weight ~ Height, mydata)  
+lmfit <- lm(Dilution.Factor ~ Ct, mydata)  
 
 # place results from fit into a data frame with the tidy function
 lmtable <- broom::tidy(lmfit)
@@ -66,4 +73,92 @@ print(lmtable)
 table_file = here("results", "resulttable.rds")
 saveRDS(lmtable, file = table_file)
 
-  
+####################################################################
+#Data Exploration More:
+######################
+
+#Summary table based on 
+SummaryTableAll<-mydata%>%
+  group_by(EXP,Target, Dilution.Factor, Method, Type)%>%
+  summarise(n=n(),
+            Mean= mean(Ct),
+            SD= sd(Ct))
+
+print(summary(SummaryTableAll))
+
+#attempt to see if i can get a better summary table?
+summary_df2 = data.frame(do.call(cbind, lapply(SummaryTableAll, summary)))
+#save data frame table to file for later use in manuscript
+summarytable_file = here("results", "summarytable.rds")
+saveRDS(summary_df2, file = summarytable_file) #not that useful
+
+#Barplot- showing total number samples
+SummaryTableAll%>%
+  ggplot(aes())+
+  geom_bar(aes(Method, fill=Type))+
+  facet_grid(Target~EXP)
+
+##Direct comparisons for input at 300uL(the same inputs for extraction)
+DirectCompare<-mydata%>%
+  filter(Input=="300")
+str(DirectCompare)
+
+print(summary(DirectCompare))
+
+p2<-DirectCompare%>%
+  ggplot(aes(Dilution.Factor,Ct))+
+  geom_jitter(aes(color=Method, shape=Type))+
+  facet_grid(Target~EXP)
+
+#look at figure
+plot(p2)
+
+#save figure
+figure_file = here("results","resultfigure2.png")
+ggsave(filename = figure_file, plot=p2) 
+## Interesting-- why is there such a group difference in Surface Sampling experiements versus Spike
+## Oh makes sense it's SD- the serial dilution. We expect the SD to be like the standard curve and the recovery should have loss.
+
+### Deeper dive into looking at the Surface Sampling Experiment, as it has some differences and is new-er data. Also includes data with Temp & RH:
+
+p3<-DirectCompare%>%
+  filter(Dilution.Factor!="NA",EXP=="SurfaceSampling", Input=="300")%>% #filter data specifically for SurfaceSampling experiment and the inputs of 300
+  ggplot(aes(x=Method, y=Ct))+
+  geom_boxplot(outlier.shape = "diamond", #cool, figured out outliers shape
+               outlier.color="red",#cool, figured out outliers color highlighter
+               aes(fill=Method))+ 
+  facet_grid(Target+Type~Dilution.Factor)
+###look at figure
+plot(p3)
+
+###save figure
+figure_file = here("results","SS300.png")
+ggsave(filename = figure_file, plot=p3)
+
+
+#### Let's focus only on the Surface sampling 
+DirectCompare%>%
+  filter(EXP=="SurfaceSampling", 
+         Input=="300")%>% 
+  ggplot(aes(x=Dilution.Factor, y=Ct))+
+  geom_jitter()+
+  geom_smooth(method="loess")+ #added smooth regression line
+  facet_grid(.~Type)
+
+             
+
+#### Try Density plots
+mydata%>%
+  filter(EXP=="SurfaceSampling", 
+         Input=="300")%>% 
+  ggplot(aes(x=Ct, color=Method))+
+  geom_density()+
+  facet_grid(.~Type)
+
+#### Try stat_ecdf()
+mydata%>%
+  filter(EXP=="SurfaceSampling", 
+         Input=="300")%>% 
+  ggplot(aes(x=Ct, color=Method))+
+  stat_ecdf()+
+  facet_grid(Dilution.Factor~Type)
